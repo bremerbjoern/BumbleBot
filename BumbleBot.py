8b1f7af54eddc3a7870598bf628564de1559ee4e
bremerbjoern <S.B.Cadell@gmail.com>
2020-07-28 22:53:19 +0200

    Update BumbleBot.py

--- BumbleBot.py
+++ BumbleBot.py
@@ -1,1 +1,17 @@
-print("hello")
+client = discord.Client()  # starts the discord client.
+
+
+@client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
+async def on_ready():  # method expected by client. This runs once when connected
+    print(f'We have logged in as {client.user}')  # notification of login.
+
+
+@client.event
+async def on_message(message):  # event that happens per any message.
+
+    # each message has a bunch of attributes. Here are a few.
+    # check out more by print(dir(message)) for example.
+    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
+
+client.run(737767239180091403)  # recall my token was saved!
